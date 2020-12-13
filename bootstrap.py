6b144f0ad4694f2ea87fa2b4e10554914641493f
AllinolCP <56921534+AllinolCP@users.noreply.github.com>
2020-12-13 20:59:00 +0700

    Update bootstrap.py

--- bootstrap.py
+++ bootstrap.py
@@ -4,16 +4,15 @@ import asyncio
 import logging
 
 from proxy import ProxyServer
-
+#i just took Houdini's bootstrap.py lool
 if __name__ == '__main__':
 
-    parser = argparse.ArgumentParser(description='Boot a Houdini server')
+    parser = argparse.ArgumentParser(description='Boot a Proxy server')
     parser.add_argument('type', action='store', default='login',
                         choices=['login', 'world'], help='Name of the server to boot')
 
-    parser.add_argument('-n', '--name', action='store', help='Houdini server name')
     parser.add_argument('-a', '--address', action='store', default='0.0.0.0',
-                        help='Houdini server address')
+                        help='Proxy server address')
     parser.add_argument('-p', '--port', action='store', help='Proxy server port', default=None, type=int)
     parser.add_argument('-s', '--server', action='store', help='Proxy server Destination', default='newcp.net', type=str)
 
